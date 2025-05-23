00c0b0934e52b2da1d6a9214723a21eaf4275b11
fisheeesh <swanphyo444@gmail.com>
2025-05-23 17:15:26 +0700

    feat: added responsive sheet menu with shadcn/ui

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button"
 import { APP_NAME } from "@/lib/constants"
-import { ShoppingCart, UserIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-import ModeToggle from "./mode-toggle"
+import Menu from "./menu"
 
 export default function Header() {
     return (
@@ -15,19 +13,7 @@ export default function Header() {
                         <span className="hidden font-bold text-2xl ml-3 lg:block">{APP_NAME}</span>
                     </Link>
                 </div>
-                <div className="space-x-2">
-                    <ModeToggle />
-                    <Button asChild variant='ghost'>
-                        <Link href='/cart'>
-                            <ShoppingCart /> Cart
-                        </Link>
-                    </Button>
-                    <Button asChild>
-                        <Link href='/sign-in'>
-                            <UserIcon /> Sign In
-                        </Link>
-                    </Button>
-                </div>
+                <Menu />
             </div>
         </header>
     )
