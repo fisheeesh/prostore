8ff0244ee006ad0dad623d7285aec7761b8f7ee8
fisheeesh <swanphyo444@gmail.com>
2025-05-23 15:22:27 +0700

    chore: shadcn/ui setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function page() {
   return (
-    <>
+    <Button>
       Prostore
-    </>
+    </Button>
   )
 }
