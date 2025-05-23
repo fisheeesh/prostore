c3e1570fb375df674a21219f1a5dce5e4f3df502
fisheeesh <swanphyo444@gmail.com>
2025-05-23 15:51:29 +0700

    chore: setting up root layout and have some constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,25 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from '@/lib/constants'
 
 const inter = Inter({
   subsets: ['latin'],
 })
 
 export const metadata: Metadata = {
-  title: "Prostore",
-  description: "A modern e-commerce platform built with Next.js",
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: `${APP_DESCRIPTION}`,
+  metadataBase: new URL(SERVER_URL)
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
+      <body className={`${inter.className} antialiased`}>
         {children}
       </body>
     </html>
