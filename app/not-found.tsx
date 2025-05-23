cb85853106eea317a0c0fd5d2218f78ab1521aef
fisheeesh <swanphyo444@gmail.com>
2025-05-23 17:02:42 +0700

    feat: design not found & loading page

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { APP_NAME } from "@/lib/constants"
+import Image from "next/image"
+
+export default function NotFoundPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <Image src='/images/logo.svg' alt={`${APP_NAME} logo`} height={48} width={48} priority={true} />
+            <div className="p-6 rounded-lg drop-shadow-md shadow mt-4 text-center w-1/3">
+                <h1 className="text-3xl font-bold mb-4">Not Found.</h1>
+                <p className="text-destructive">Could not find requested page :(</p>
+                <Button variant='outline' className="mt-4 ml-2" onClick={() => window.location.href = '/'}>
+                    Back to Home.
+                </Button>
+            </div>
+        </div>
+    )
+}
