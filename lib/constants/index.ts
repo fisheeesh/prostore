ef37492d4dc7d9d75335151740eddabcc21c5c06
fisheeesh <swanphyo444@gmail.com>
2025-05-25 18:52:15 +0700

    feat: hook up sign up form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,6 +3,12 @@ export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A mod
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.NEXT_LATEST_PRODUCTS_LIMIT) || 4
 export const SIGN_IN_DEFAULT_VALUES = {
-    email: '',
-    password: ''
+    email: 'admin@prostore.com',
+    password: '123456'
+}
+export const SIGN_UP_DEFAULT_VALUES = {
+    name: 'test',
+    email: 'test@example.com',
+    password: '123456',
+    confirmPassword: '123456'
 }
