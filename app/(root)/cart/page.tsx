e4eefc3ecc8afcfd51baa03a4091120815ff75df
fisheeesh <swanphyo444@gmail.com>
2025-05-27 16:40:26 +0700

    feat: create cart page

---
+++ app/(root)/cart/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from 'next'
+import React from 'react'
+import CartTable from './cart-table'
+import { getMyCart } from '@/lib/actions/cart.actions'
+
+export const metadata: Metadata = {
+    title: 'Shopping Cart'
+}
+
+export default async function CartPage() {
+    const cart = await getMyCart()
+
+    return (
+        <>
+            <CartTable cart={cart} />
+        </>
+    )
+}
