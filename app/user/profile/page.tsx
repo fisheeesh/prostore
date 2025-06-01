49609a2192c56a52290acf59e8d65486404cd463
fisheeesh <swanphyo444@gmail.com>
2025-06-02 03:07:27 +0700

    chore: create update profile form

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { auth } from '@/auth'
+import { Metadata } from 'next'
+import { SessionProvider } from 'next-auth/react'
+import ProfileForm from './profile-form'
 
-export default function ProfilePage() {
+/**
+ * * In client component, we can simply say const session = await auth()
+ * * Instead we have to use a hook called useSession()
+ * * In order to use that wherever that form is embedded, that component -- it has to be wrapped in SessionProvider
+ */
+export const metadata: Metadata = {
+    title: "My Profile"
+}
+
+export default async function ProfilePage() {
+    const session = await auth()
+
     return (
-        <div>
-            pp
-        </div>
+        <SessionProvider session={session}>
+            <div className="max-w-md mx-auto space-y-4">
+                <h2 className="h2-bold">Profile</h2>
+                <ProfileForm />
+            </div>
+        </SessionProvider>
     )
 }
---
+++ app/user/profile/profile-form.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useToast } from "@/hooks/use-toast"
+import { updateProfileSchema } from "@/lib/validator"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Loader } from "lucide-react"
+import { useSession } from "next-auth/react"
+import { ControllerRenderProps, SubmitHandler, useForm } from "react-hook-form"
+import { z } from "zod"
+
+export default function ProfileForm() {
+    const { data: session, update } = useSession()
+
+    const form = useForm<z.infer<typeof updateProfileSchema>>({
+        resolver: zodResolver(updateProfileSchema),
+        defaultValues: {
+            name: session?.user?.name ?? '',
+            email: session?.user?.email ?? ''
+        }
+    })
+
+    const { toast } = useToast()
+
+
+    const onSubmit: SubmitHandler<z.infer<typeof updateProfileSchema>> = async (values) => {
+
+    }
+
+    return (
+        <Form {...form}>
+            <form action="" className="flex flex-col gap-2" onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="flex flex-col-gap-5">
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }: { field: ControllerRenderProps<z.infer<typeof updateProfileSchema>, 'email'> }) => (
+                            <FormItem className="w-full">
+                                <FormLabel>Email <span className="text-red-600">*</span></FormLabel>
+                                <FormControl>
+                                    <Input disabled placeholder='Email' className="input-field" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div className="flex flex-col gap-5">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }: { field: ControllerRenderProps<z.infer<typeof updateProfileSchema>, 'name'> }) => (
+                            <FormItem className="w-full">
+                                <FormLabel>Name <span className="text-red-600">*</span></FormLabel>
+                                <FormControl>
+                                    <Input placeholder='Name' className="input-field" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <Button disabled={form.formState.isSubmitting} type="submit" size='lg' className="button mt-3 col-span-2 w-full">
+                    {form.formState.isSubmitting ? (
+                        <>
+                            <Loader className="h-4 w-4 animate-spin" /> Submitting..
+                        </>
+                    ) : 'Update Profile'}
+                </Button>
+            </form>
+        </Form>
+    )
+}
