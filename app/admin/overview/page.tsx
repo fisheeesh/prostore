da0a5985fe4ab2fa57542b64caebe68e649469f8
fisheeesh <swanphyo444@gmail.com>
2025-06-02 18:52:57 +0700

    feat: added montyly sales charts using recharts

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -6,6 +6,7 @@ import { formatCurrency, formatDateTime, formatNumber } from "@/lib/utils"
 import { BadgeDollarSign, Barcode, CreditCard, Users } from "lucide-react"
 import { Metadata } from "next"
 import Link from "next/link"
+import Charts from "./charts"
 
 export const medatdata: Metadata = {
     title: 'Admin Dashboard'
@@ -75,7 +76,7 @@ export default async function AdminOverviewPage() {
                         <CardTitle>Overview</CardTitle>
                     </CardHeader>
                     <CardContent>
-                        {/* Charts */}
+                        <Charts data={{ salesData: summary.salesData }} />
                     </CardContent>
                 </Card>
                 <Card className="col-span-3">
