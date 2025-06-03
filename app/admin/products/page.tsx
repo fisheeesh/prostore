3aad6c35849483f1dc8c5dd8d41ef4a3a054a4e6
fisheeesh <swanphyo444@gmail.com>
2025-06-03 15:35:06 +0700

    chore: displayed products in admin's products page

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,4 +1,9 @@
+import Pagination from '@/components/shared/pagination'
+import { Button } from '@/components/ui/button'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { getAllProductsAction } from '@/lib/actions/product.actions'
+import { formatCurrency, formatId } from '@/lib/utils'
+import Link from 'next/link'
 import React from 'react'
 
 export default async function AdminProductsPage(props: { searchParams: Promise<{ page: string, query: string, category: string }> }) {
@@ -13,13 +18,46 @@ export default async function AdminProductsPage(props: { searchParams: Promise<{
         category: cateogry
     })
 
-    console.log(products)
-
     return (
         <div className='space-y-2'>
             <div className="flex-between">
                 <h1 className="h2-bold">Products</h1>
+                <Button asChild variant='default'>
+                    <Link href='/admin/products/create'>Create Product</Link>
+                </Button>
             </div>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className='whitespace-nowrap'>ID</TableHead>
+                        <TableHead className='whitespace-nowrap'>NAME</TableHead>
+                        <TableHead className='text-right whitespace-nowrap'>PRICE</TableHead>
+                        <TableHead className='whitespace-nowrap'>CATEGORY</TableHead>
+                        <TableHead className='whitespace-nowrap'>STOCK</TableHead>
+                        <TableHead className='whitespace-nowrap'>RATING</TableHead>
+                        <TableHead className='w-[100px] whitespace-nowrap'>ACTIONS</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {products.data.map((product) => (
+                        <TableRow key={product.id}>
+                            <TableCell className='whitespace-nowrap'>{formatId(product.id)}</TableCell>
+                            <TableCell className='whitespace-nowrap'>{product.name}</TableCell>
+                            <TableCell className='text-right whitespace-nowrap'>{formatCurrency(product.price)}</TableCell>
+                            <TableCell className='whitespace-nowrap'>{product.category}</TableCell>
+                            <TableCell className='whitespace-nowrap'>{product.stock}</TableCell>
+                            <TableCell className='whitespace-nowrap'>{product.rating}</TableCell>
+                            <TableCell className='flex items-center gap-1'>
+                                <Button asChild variant='outline' size='sm'>
+                                    <Link href={`/admin/products/${product.id}`}>Edit</Link>
+                                </Button>
+                                {/* Delete  Button*/}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            {!!products.totalPages && products.totalPages > 1 && <Pagination page={page} totalPages={products.totalPages} />}
         </div>
     )
 }
