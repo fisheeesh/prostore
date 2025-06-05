e3153d8a4825e1db6ad78af8f525786f57ae99f5
fisheeesh <swanphyo444@gmail.com>
2025-06-05 15:28:23 +0700

    chore: all view all products btn to navigate search page

---
+++ components/view-all-products-btn.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
+
+export default function ViewAllProductsBtn() {
+    return (
+        <div className='flex items-center justify-center my-8'>
+            <Button className='px-8 py-4 text-lg font-semibold' asChild>
+                <Link href='/search'>View All Products</Link>
+            </Button>
+        </div>
+    )
+}
