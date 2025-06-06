9bed2fb5be71d2241da56dd1e608c0b124ac75fe
fisheeesh <swanphyo444@gmail.com>
2025-06-06 16:10:04 +0700

    chore: created review list component

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth"
 import AddToCart from "@/components/shared/product/add-to-cart"
 import ProductImages from "@/components/shared/product/product-images"
 import ProductPrice from "@/components/shared/product/product-price"
@@ -6,6 +7,7 @@ import { Card, CardContent } from "@/components/ui/card"
 import { getMyCart } from "@/lib/actions/cart.actions"
 import { getLatestProductsAction, getProductBySlugAction } from "@/lib/actions/product.actions"
 import { notFound } from "next/navigation"
+import ReviewList from "./review-list"
 
 export async function generateMetadata(props: { params: Promise<{ slug: string }> }) {
     const { slug } = await props.params
@@ -25,6 +27,8 @@ export const generateStaticParams = async () => {
 
 export default async function ProductDetailPage(props: { params: Promise<{ slug: string }> }) {
     const { slug } = await props.params
+    const session = await auth()
+    const userId = session?.user?.id
 
     const product = await getProductBySlugAction(slug)
 
@@ -33,68 +37,73 @@ export default async function ProductDetailPage(props: { params: Promise<{ slug:
     const cart = await getMyCart()
 
     return (
-        <section className="">
-            <div className="grid grid-cols-1 md:grid-cols-5">
-                {/* Images Column */}
-                <div className="col-span-2">
-                    <ProductImages images={product.images} />
-                </div>
-                {/* Detials Column */}
-                <div className="col-span-2 p-5">
-                    <div className="flex flex-col gap-6">
-                        <p>{product.brand} {product.category}</p>
-                        <h3 className="h3-bold">{product.name}</h3>
-                        <p>{product.rating} of {product.numReviews} Reviews.</p>
-                        <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
-                            <ProductPrice value={Number(product.price)} className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2" />
+        <>
+            <section>
+                <div className="grid grid-cols-1 md:grid-cols-5">
+                    {/* Images Column */}
+                    <div className="col-span-2">
+                        <ProductImages images={product.images} />
+                    </div>
+                    {/* Detials Column */}
+                    <div className="col-span-2 p-5">
+                        <div className="flex flex-col gap-6">
+                            <p>{product.brand} {product.category}</p>
+                            <h3 className="h3-bold">{product.name}</h3>
+                            <p>{product.rating} of {product.numReviews} Reviews.</p>
+                            <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+                                <ProductPrice value={Number(product.price)} className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2" />
+                            </div>
+                        </div>
+                        <div className="mt-10">
+                            <p className="font-semibold">Description</p>
+                            <p>{product.description}</p>
                         </div>
                     </div>
-                    <div className="mt-10">
-                        <p className="font-semibold">Description</p>
-                        <p>{product.description}</p>
-                    </div>
-                </div>
-                {/* Action Column */}
-                <div className="col-span-1">
-                    <Card>
-                        <CardContent className="p-4">
-                            <div className="mb-2 flex justify-between items-center">
-                                <div>Price</div>
-                                <div>
-                                    <ProductPrice value={Number(product.price)} />
+                    {/* Action Column */}
+                    <div className="col-span-1">
+                        <Card>
+                            <CardContent className="p-4">
+                                <div className="mb-2 flex justify-between items-center">
+                                    <div>Price</div>
+                                    <div>
+                                        <ProductPrice value={Number(product.price)} />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="mb-2 flex justify-between items-center">
-                                <div>Status</div>
+                                <div className="mb-2 flex justify-between items-center">
+                                    <div>Status</div>
+                                    {
+                                        product.stock > 0 ? (
+                                            <Badge variant='outline'>In Stock</Badge>
+                                        ) : (
+                                            <Badge variant='destructive'>Out of Stock</Badge>
+                                        )
+                                    }
+                                </div>
                                 {
-                                    product.stock > 0 ? (
-                                        <Badge variant='outline'>In Stock</Badge>
-                                    ) : (
-                                        <Badge variant='destructive'>Out of Stock</Badge>
+                                    product.stock > 0 && (
+                                        <div className="flex-center mt-5">
+                                            <AddToCart
+                                                cart={cart}
+                                                item={{
+                                                    productId: product.id,
+                                                    name: product.name,
+                                                    slug: product.slug,
+                                                    qty: 1,
+                                                    image: product.images![0],
+                                                    price: product.price
+                                                }} />
+                                        </div>
                                     )
                                 }
-                            </div>
-                            {
-                                product.stock > 0 && (
-                                    <div className="flex-center mt-5">
-                                        <AddToCart
-                                            cart={cart}
-                                            item={{
-                                                productId: product.id,
-                                                name: product.name,
-                                                slug: product.slug,
-                                                qty: 1,
-                                                image: product.images![0],
-                                                price: product.price
-                                            }} />
-                                    </div>
-                                )
-                            }
-                        </CardContent>
-                    </Card>
+                            </CardContent>
+                        </Card>
+                    </div>
                 </div>
-            </div>
-
-        </section>
+            </section>
+            <section className="mt-10">
+                <h2 className="h2-bold">Customer Reviews</h2>
+                <ReviewList userId={userId || ''} productId={product.id} productSlug={slug} />
+            </section>
+        </>
     )
 }
---
+++ app/(root)/product/[slug]/review-list.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Review } from "@/types"
+import Link from "next/link"
+import { useState } from "react"
+
+export default function ReviewList({ userId, productId, productSlug }: { userId: string, productId: string, productSlug: string }) {
+    const [reviews, setReviews] = useState<Review[]>([])
+
+    return (
+        <div className="space-y-4">
+            {
+                reviews.length === 0 && (
+                    <div>No Reviews yet.</div>
+                )
+            }
+            {
+                userId ? (
+                    <>
+                    </>
+                ) : (
+                    <div>
+                        Please <Link className="text-yellow-500 px-1" href={`/sign-in?callbackUrl=/product/${productSlug}`}>Sign In</Link>
+                        to write a review.
+                    </div >
+                )
+            }
+            <div className="flex flex-col gap-3">
+                Reviews
+            </div>
+        </div>
+    )
+}
