32745c39dd6c456905e152e72d10bd0df1c02b45
fisheeesh <swanphyo444@gmail.com>
2025-06-07 16:39:10 +0700

    feat: implemented order form payment intent for stripe payment

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation"
 import OrderDetailsTable from "./order-details-table"
 import { ShippingAddress } from "@/types"
 import { auth } from "@/auth"
+import Stripe from 'stripe'
 
 export const metadata: Metadata = {
     title: 'Order Details'
@@ -17,12 +18,30 @@ export default async function OrderDetailsPage(props: { params: Promise<{ id: st
 
     if (!order) notFound()
 
+    let client_secret = null
+
+    //* Check if it is not paid and using Stripe
+    if (order.paymentMethod === 'Stripe' && !order.isPaid) {
+        //* init stripe instance
+        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+
+        //* Create a payment intent
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: Math.round(Number(order.totalPrice) * 100),
+            currency: 'USD',
+            metadata: { orderId: order.id }
+        })
+
+        client_secret = paymentIntent.client_secret
+    }
+
     return (
         <>
             <OrderDetailsTable order={{
                 ...order,
                 shippingAddress: order.shippingAddress as ShippingAddress
             }}
+                stripeClientSecret={client_secret}
                 paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
                 isAdmin={session?.user?.role === 'admin' || false}
             />
