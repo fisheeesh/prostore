450646515fd2ac6276eeea7f52d0f3a72b55436d
fisheeesh <swanphyo444@gmail.com>
2025-06-10 16:30:51 +0700

    chore: create preview props for purchase receipt

--- email/purchase-receipt.tsx
+++ email/purchase-receipt.tsx
@@ -14,10 +14,60 @@ import {
 } from "@react-email/components"
 import { Order } from '@/types'
 import { formatCurrency } from "@/lib/utils"
+import sampleData from "@/db/sample-data"
+
+require('dotenv').config()
+
+PurchaseReceiptEmail.PreviewProps = {
+    order: {
+        id: crypto.randomUUID(),
+        userId: '123',
+        user: {
+            name: 'John Doe',
+            email: "text@test.com"
+        },
+        paymentMethod: 'Stripe',
+        shippingAddress: {
+            fullName: 'John Doe',
+            streetAddress: '123 Main St',
+            city: 'New York',
+            postalCode: '12345',
+            country: 'US'
+        },
+        createdAt: new Date(),
+        totalPrice: '100',
+        taxPrice: '10',
+        shippingPrice: '10',
+        itemsPrice: '80',
+        orderitems: sampleData.products.map(x => ({
+            name: x.name,
+            orderId: '123',
+            productId: '123',
+            slug: x.slug,
+            qty: x.stock,
+            image: x.images[0],
+            price: x.price.toString()
+        })),
+        isDelivered: true,
+        deliveredAt: new Date(),
+        isPaid: true,
+        paidAt: new Date(),
+        paymentResult: {
+            id: '123',
+            status: 'succeeded',
+            pricePaid: '100',
+            email_address: 'text@test.com'
+        }
+    }
+} satisfies OrderInformationProps
 
 const dateFormatter = new Intl.DateTimeFormat('en', { dateStyle: 'medium' })
 
-export default function PurchaseReceiptEmail({ order }: { order: Order }) {
+type OrderInformationProps = {
+    order: Order
+}
+
+export default function PurchaseReceiptEmail({ order }: OrderInformationProps) {
     return (
         <Html>
             <Preview>View Order Receipt</Preview>
@@ -86,7 +136,7 @@ export default function PurchaseReceiptEmail({ order }: { order: Order }) {
                                     <Row key={name} className="py-1">
                                         <Column align="right">{name}: </Column>
                                         <Column align="right" width={70} className="align-top">
-                                            <Text className="mt-0">
+                                            <Text className="m-0">
                                                 {formatCurrency(price)}
                                             </Text>
                                         </Column>
