3ac1d1b5b407379a6d81dcd508f616c818bac699
fisheeesh <swanphyo444@gmail.com>
2025-06-27 02:56:43 +0700

    fix: format product price

--- components/shared/product/product-price.tsx
+++ components/shared/product/product-price.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { cn, formatNumber } from "@/lib/utils";
 
 export default function ProductPrice({ value, discount, className }: { value: number; discount?: number; className?: string; }) {
     //* Ensure 2 decimal places
@@ -12,13 +12,13 @@ export default function ProductPrice({ value, discount, className }: { value: nu
                 discount ?
                     <p className={cn('text-2xl', className)}>
                         <span className="text-xs align-super">$</span>
-                        <span className="font-bold">{intValue}</span>
+                        <span className="font-bold">{formatNumber(Number(intValue))}</span>
                         <span className="text-xs align-super">.{floatValue}</span>
                         <span className="line-through text-muted-foreground text-xs ml-1">${value}</span>
                     </p> :
                     <p className={cn('text-2xl', className)}>
                         <span className="text-xs align-super">$</span>
-                        <span className="font-bold">{intValue}</span>
+                        <span className="font-bold">{formatNumber(Number(intValue))}</span>
                         <span className="text-xs align-super">.{floatValue}</span>
                     </p>
             }
